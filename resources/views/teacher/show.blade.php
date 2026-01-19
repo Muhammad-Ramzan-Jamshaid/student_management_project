621dd94c951a4a2c3658ea4139d88342b7b0cabb
Muhammad Ramzan Jamshaid <ramzanjamshaid8u@gmail.com>
2026-01-19 08:45:56 -0800

    adding course migration

--- resources/views/teacher/show.blade.php
+++ resources/views/teacher/show.blade.php
@@ -12,7 +12,7 @@
             <p class="card-text">Email : {{ $studnet->email }}</p>
         <p class="card-text">Address : {{ $teacher->address }}</p>
         <p class="card-text">Phone : {{ $teacher->phone }}</p>
-        <p class="card-text">Age : {{ $teacher->age}}</p>
+        <p class="card-text">Age : {{ $teacher->age}}</p>show
         <p class="card-text">Experience : {{ $teacher->experience}}</p>
   </div>
        
